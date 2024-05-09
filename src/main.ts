baf84143e3320acdfdb46eefee62e0c524a4a391
mizuho1998 <ca1375@cartahd.com>
2024-05-10 00:02:56 +0900

    tmp

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function main() {
       throw new Error(`setup-xcode supports only macOS, current host is ${runnerOs}`)
     }
     const input = getInput()
+    core.info('!!!!!!!!!!!!!')
     core.info('Input parameters:')
     Object.entries(input).forEach(([key, value]) => {
       core.info(`  ${key} = ${value}`)
